a0ee3ceaaa808fcee8a9e176a4eeef0019396232
krgirard33 <ken.girard@gmail.com>
2017-03-12 16:19:25 -0500

    Added in PrintArray

---
+++ PrintArray/src/PrintArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] someInts = { 1, 1, 2, 3, 5, 8 };
+            foreach (int num in someInts)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
