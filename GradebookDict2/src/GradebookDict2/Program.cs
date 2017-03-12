8bfa909123c44c32392b40e227d9bdeadc825d40
krgirard33 <ken.girard@gmail.com>
2017-03-12 17:40:27 -0500

    Added in GradebookDict2

---
+++ GradebookDict2/src/GradebookDict2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradebookDict
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<int, string> students =
+                new Dictionary<int, string>();
+            int studentID = 1;
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.Write("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    // Get the student's grade
+                    students.Add(studentID, newStudent);
+                    studentID = ++studentID;
+                }
+            }
+            while (newStudent != "");
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine("(" + student.Key + ") " + student.Value);
+            }    
+
+            Console.ReadLine();
+        }
+    }
+}
